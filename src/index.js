55abe511c7e11ef4d49bbfdd835b2c8054a1071c
DaryaGorbachova <vrindavaneshvari@mail.ru>
2021-08-11 22:57:58 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  let result = array[0];
+	for (let i = 1; i < array.length; i++) {
+		if (result > array[i]) result = array[i];
+	}
+	return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let result = 0;
+	for (let i = 0; i < array.length; i++) {
+		if (result < array[i]) result = array[i];
+	}
+	return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = 0;
+	let sum = 0;
+	for (let i = 0; i < array.length; i++) {
+		sum += array[i];
+	}
+	result = sum / array.length
+	return result;
 }
